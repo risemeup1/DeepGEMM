6552bd13647d6ed5babca30a0dda39cac29b2d4c
risemeup1 <515586620@qq.com>
2025-02-27 20:03:30 +0800

    fix

--- deep_gemm/jit_kernels/__init__.py
+++ deep_gemm/jit_kernels/__init__.py
@@ -1,8 +1,4 @@
 from .gemm import gemm_fp8_fp8_bf16_nt
-from .m_grouped_gemm import (
-    m_grouped_gemm_fp8_fp8_bf16_nt_contiguous,
-    m_grouped_gemm_fp8_fp8_bf16_nt_masked
-)
 from .utils import (
     cell_div, set_num_sms, get_num_sms,
     get_col_major_tma_aligned_tensor,
